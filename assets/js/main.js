610371004b405fe021a2d66474e83d1de83c1153
Table Six <tablesix@ZARATHUSTRA.local>
2018-02-22 01:48:46 +0700

    init

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(window).bind("load", function() {
+	var i = 1;
+	var iv = 1;
+	while(i < 4){
+		setTimeout(function(){
+			$(".sec-"+iv).removeClass('sec-'+iv);
+			console.log(iv);
+			iv++
+		},500 + (i * 700))
+		i++;
+	}
+	
+});
